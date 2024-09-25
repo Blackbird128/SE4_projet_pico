7171dd4057a7ecbb49eb922ee749c3c8c7286ee8
pifou <pifou@zabeth12.plil.info>
2024-09-25 15:45:47 +0200

    fin de première séance ordonnanceur

---
+++ PicoCode/Ordonnanceur/ordo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <avr/io.h>
+
+#define portCLEAR_COUNTER_ON_MATCH ( 0x08 )
+#define portPRESCALE_256 ( 0x04 )
+#define portCLOCK_PRESCALER ( 256 )
+#define portCOMPARE_MATCH_A_INTERRUPT_ENABLE ( 0x10 )
+
+static void prvSetupTimerInterrupt( void )
+{
+unsigned portLONG ulCompareMatch;
+unsigned portCHAR ucHighByte, ucLowByte;
+/* Generate the compare match value for our required tick
+frequency. */
+ulCompareMatch = portCPU_CLOCK_HZ / portTICK_RATE_HZ;
+EXAMPLE C IMPLEMENTATION OF A MULTITASKING KERNEL FOR THE AVR, RICHARD BARRY - AVRFREAKS.NET
+4
+/* We only have 16 bits so have to scale to get our
+required tick rate. */
+ulCompareMatch /= portCLOCK_PRESCALER;
+/* Setup compare match value for compare match A.
+Interrupts are disabled before calling this function so
+we need not bother here. [casting has been removed for
+each of reading] */
+ucLowByte = ulCompareMatch & 0xff;
+ulCompareMatch >>= 8;
+ucHighByte = ulCompareMatch & 0xff;
+outb( OCR1AH, ucHighByte );
+outb( OCR1AL, ucLowByte );
+/* Setup clock source and compare match behaviour. */
+ucLowByte = portCLEAR_COUNTER_ON_MATCH | portPRESCALE_256;
+outb( TCCR1B, ucLowByte );
+/* Enable the interrupt - this is okay as interrupt
+are currently globally disabled. */
+ucLowByte = inb( TIMSK );
+ucLowByte |= portCOMPARE_MATCH_A_INTERRUPT_ENABLE;
+outb( TIMSK, ucLowByte );
+}
+ISR(TIMER1_COMPA_vect,ISR_NAKED)
+{
+/* Sauvegarde du contexte de la tâche interrompue */
+...
+/* Appel à l'ordonnanceur */
+scheduler();
+/* Récupération du contexte de la tâche ré-activée */
+...
+asm volatile ( "reti" );
+}
