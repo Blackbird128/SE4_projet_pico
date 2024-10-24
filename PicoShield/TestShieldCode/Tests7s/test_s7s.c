0f50dd430871305ff95bb63866040ea94512951d
Louis Bonningre <louis.bonningre.etu@univ-lille.fr>
2024-10-24 16:01:37 +0200

    Premier Schema

--- PicoShield/TestShieldCode/Tests7s/test_s7s.c
+++ PicoShield/TestShieldCode/Tests7s/test_s7s.c
@@ -1,35 +1,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SS_PIN PD7 //Port J6 !!
-#define MOSI_PIN PB3
-#define SCK_PIN PB5
+#define SS 2
+#define MOSI 3
+#define SCK 5
+
+#define DISPLAY_DDR DDRD
+#define DISPLAY_PORT PORTD
+#define DISPLAY_PIN 7
 
 
 void SPI_init() {
-    DDRB |= (1 << MOSI_PIN) | (1 << SCK_PIN);
-    DDRD |= (1 << SS_PIN);
-    PORTD |= (1 << SS_PIN);
+    DDRB |= (1 << MOSI) | (1 << SCK) | (1<<SS);
+    PORTB |= (1<<SS);
     SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR1);                 // Activation SPI (SPE) en état maître (MSTR)
                                                          // horloge F_CPU/64 (SPR1=1,SPR0=0)
 }
 
+void S7S_init() {
+    DISPLAY_DDR |= (1<<DISPLAY_PIN);
+}
+
 void SPI_send(uint8_t data) {
-    PORTD &= ~(1 << SS_PIN);
+    DISPLAY_PORT &= ~(1<<DISPLAY_PIN);
     SPDR = data;
     while (!(SPSR & (1 << SPIF)));
-    PORTD |= (1 << SS_PIN);
+    DISPLAY_PORT |= (1<<DISPLAY_PIN);
 }
 
 void clearDisplaySPI() {
-    PORTD &= ~(1 << SS_PIN);
     SPI_send(0x76); // Commande pour effacer l'affichage
-    PORTD |= (1 << SS_PIN);
 }
-
 
 int main(void) {
     SPI_init();
+    S7S_init();
     clearDisplaySPI();
     int cmp = 0;
 
